9fad4df32683569e74c90f35f34c919730ddead5
mjevtic <mjevtic@ik.me>
2025-06-12 00:50:40 +0200

    Connection to database and table Rule

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlmodel.ext.asyncio.session import AsyncSession
+from db import async_session, init_db
+from models import Rule
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
 
 @app.get("/")
 def read_root():
