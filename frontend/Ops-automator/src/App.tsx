7cce0ddb60ae7b98005c11bfd25b885f32fc4bdb
Mladen Jevtić <mjevtic@ik.me>
2025-06-11 20:42:43 +0000

    Initial commit of Ops Automator project

---
+++ frontend/Ops-automator/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-900 text-white text-2xl">
+      SupportOps Automator frontend is running 🚀
+    </div>
+  );
+}
+
+export default App;
